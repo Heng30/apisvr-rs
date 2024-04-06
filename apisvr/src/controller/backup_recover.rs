b2204ddfc588ed2e3d267c7c059bcb38ce6652b4
blueJpg <2238288979@qq.com>
2024-04-07 01:17:15 +0800

    [*] fix bug: backup content is too large

--- apisvr/src/controller/backup_recover.rs
+++ apisvr/src/controller/backup_recover.rs
@@ -4,10 +4,19 @@ use crate::response::data;
 pub mod rssbox_android {
     use super::*;
     use crate::db::RSSBOX_ANDROID_BACKUP_TABLE;
+    use rocket::data::{Data, Limits, ToByteUnit};
 
     #[post("/backup?<api_token>", format = "application/json", data = "<input>")]
-    pub async fn backup(api_token: &str, input: &str) -> data::Data {
-        com_update(RSSBOX_ANDROID_BACKUP_TABLE, api_token, input).await
+    pub async fn backup(api_token: &str, input: Data<'_>, limits: &Limits) -> data::Data {
+        let limit = limits.get("input").unwrap_or(1.mebibytes());
+        match input.open(limit).into_string().await {
+            Err(e) => data::Data::new_with_status(
+                e.to_string().as_bytes().to_vec(),
+                ContentType::Plain,
+                Status::InternalServerError,
+            ),
+            Ok(v) => com_update(RSSBOX_ANDROID_BACKUP_TABLE, api_token, &v.value).await,
+        }
     }
 
     #[get("/recover?<api_token>")]
